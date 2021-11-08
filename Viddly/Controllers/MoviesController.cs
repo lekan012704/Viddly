0b0d66b51191c517d40301d0cc9c19ce0e20d6f3
Durosinmi Abdul Sobuur <durosinmiabdulsobuur@gmail.com>
2021-11-08 07:09:10 +0100

    Add project files.

---
+++ Viddly/Controllers/MoviesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Viddly.Models;
+
+namespace Viddly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+
+        public MoviesController()
+        {
+        }
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek"
+            };
+            return View(movie);
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageindex , string sortby)
+        {
+            if (!pageindex.HasValue)
+                pageindex = 1;
+            if (String.IsNullOrWhiteSpace(sortby))
+                sortby = "Name";
+          return  Content(String.Format("pageindex = {0} &sortby = {1}", pageindex, sortby));
+
+        }
+      
+            [ActionName("Find")]
+            public ActionResult GetById(int id)
+            {
+                // get student from the database 
+                return View();
+            }
+
+        z
+        }
+
+    
+}
