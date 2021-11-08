7e1c40d7cfec58e571a0a03d12162098b58296ae
Durosinmi Abdul Sobuur <durosinmiabdulsobuur@gmail.com>
2021-11-08 07:09:10 +0100

    Add project files.

---
+++ Viddly/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Viddly.Models;
+
+namespace Viddly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek"
+            };
+            return View(movie);
+        }
+    }
+}
